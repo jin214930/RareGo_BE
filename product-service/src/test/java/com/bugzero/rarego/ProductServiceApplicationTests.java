9e83e6fcff5a6acb008a9493768d0aa4d31c75a0
pty10510 <pty10510@gmail.com>
2026-02-04 17:10:07 +0900

    test(product): test 환경에서 ChatModel빈을 Mock으로 설정하여 어플리케이션 테스트가 통과되도록
    - 왜: application Test 실행시 chatmodel이 테스트 환경에서 설정되지 않아 등록할 빈이 없어 테스트 실패되었었음 이를 방지하기 위해.
    - 무엇: test 환경에서 config 파일을 생성하여 chatmodel을 목빈으로 생성.
    
    Signed-off-by: pty10510 <pty10510@gmail.com>

--- product-service/src/test/java/com/bugzero/rarego/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/bugzero/rarego/ProductServiceApplicationTests.java
@@ -2,8 +2,12 @@ package com.bugzero.rarego;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import com.bugzero.rarego.ai.config.TestAiConfig;
 
 @SpringBootTest
+@Import(TestAiConfig.class) // 테스트용 가짜 빈 설정을 주입!
 class ProductServiceApplicationTests {
 
 	@Test
