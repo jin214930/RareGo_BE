20849ce07ad5543a546e0824b0177faa8d8bb994
[Kkimdoyeon] <ehdus5176@gmail.com>
2026-02-04 16:55:43 +0900

    feat(product): build.gradle.kts에 jacoco 코드 추가

--- product-service/build.gradle.kts
+++ product-service/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
+    jacoco
 }
 val springAiVersion by extra("2.0.0-M2")
 
@@ -69,4 +70,34 @@ dependencyManagement {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    dependsOn(tasks.test)
+    violationRules {
+        rule {
+            limit {
+                // 초기 개발 단계
+                minimum = "0.00".toBigDecimal()
+            }
+        }
+    }
 }
