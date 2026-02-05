25d5d5f02a31c22de2884de60593830628a28275
ward <rlawlsaud2149@gmail.com>
2026-02-05 11:14:22 +0900

    chore(common): RedissonConfig @ConditionalOnProperty 제거

--- common/src/main/java/com/bugzero/rarego/global/config/RedissonConfig.java
+++ common/src/main/java/com/bugzero/rarego/global/config/RedissonConfig.java
@@ -4,12 +4,10 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(name = "rarego.redisson.enabled", havingValue = "true")
 public class RedissonConfig {
 
 	@Value("${spring.data.redis.host}")
