0fd0d783a9ffd0e8a84fff630e76927af51c3370
ward <rlawlsaud2149@gmail.com>
2026-02-05 23:48:50 +0900

    feat(notification): 알림 엔티티 구현

---
+++ notification-service/src/main/java/com/bugzero/rarego/domain/Notification.java
@@ -0,0 +1,45 @@
+package com.bugzero.rarego.domain;
+
+import com.bugzero.rarego.global.jpa.entity.BaseIdAndTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "NOTIFICATION_NOTIFICATION")
+public class Notification extends BaseIdAndTime {
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(nullable = false)
+	private NotificationMember member;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String message;
+
+	@Column(nullable = false)
+	@Builder.Default
+	private boolean isRead = false;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private NotificationType type;
+
+	private String link;
+}
