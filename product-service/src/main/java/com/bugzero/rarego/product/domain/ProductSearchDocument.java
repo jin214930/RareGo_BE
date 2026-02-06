7f37165ac7dbbb90249dbc818071dbe2775a15ad
pty10510 <pty10510@gmail.com>
2026-02-06 17:33:44 +0900

    refactor(product): ProductSearchDocument 유사도 검색 기준 설정 변경
    Signed-off-by: pty10510 <pty10510@gmail.com>

--- product-service/src/main/java/com/bugzero/rarego/product/domain/ProductSearchDocument.java
+++ product-service/src/main/java/com/bugzero/rarego/product/domain/ProductSearchDocument.java
@@ -1,6 +1,7 @@
 package com.bugzero.rarego.product.domain;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
@@ -12,32 +13,37 @@ import com.bugzero.rarego.shared.auction.type.AuctionStatus;
 import com.bugzero.rarego.shared.product.type.Category;
 import com.bugzero.rarego.shared.product.type.ProductCondition;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@Builder
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @Document(indexName = "product_search")
 @Setting(settingPath = "elasticsearch-settings.json")
 public class ProductSearchDocument {
 
     @Id
-    private String id; // {productId}_{auctionId} 형태로 구성
+    private String id;
 
     @Field(type = FieldType.Long)
     private Long productId; // 원본 참조
 
     @Field(type = FieldType.Long)
-    private Long auctionId; // 원본 참조 (미등록 시 null)
+    // 경매 이력 추적이 중요한가?
+    private Long auctionId; // 원본 참조
 
     @Field(type = FieldType.Text, analyzer = "nori")
     private String productName;
 
+    @Field(type = FieldType.Text, analyzer = "nori")
+    private String description;
+
     @Field(type = FieldType.Keyword)
     private ProductCondition productCondition;
-
-    @Field(type = FieldType.Dense_Vector, dims = 1536)
-    private float[] descriptionVector; // 유사도 검색
 
     @Field(type = FieldType.Keyword)
     private Category category;
@@ -56,4 +62,15 @@ public class ProductSearchDocument {
 
     @Field(type = FieldType.Date)
     private LocalDateTime closedAt;
+
+    // 경매 쪽 상품 정보 통신 위해서 필요
+    @Field(type = FieldType.Long)
+    private Long sellerId;
+
+    @Field(type = FieldType.Keyword)
+    private String imageUrl;
+
+    // 상품명+설명 벡터
+    @Field(type = FieldType.Dense_Vector, dims = 1536, similarity = "cosine", index = true)
+    private List<Float> embedding;
 }
