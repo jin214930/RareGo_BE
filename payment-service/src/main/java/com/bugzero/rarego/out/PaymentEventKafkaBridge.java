a31e653ee9ce4532ae4ad6bcf0b4f7addbfb8ad8
ward <rlawlsaud2149@gmail.com>
2026-02-06 14:22:53 +0900

    feat(payment): AuctionPaymentCompletedEvent 카프카 발행

--- payment-service/src/main/java/com/bugzero/rarego/out/PaymentEventKafkaBridge.java
+++ payment-service/src/main/java/com/bugzero/rarego/out/PaymentEventKafkaBridge.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+import com.bugzero.rarego.shared.payment.event.AuctionPaymentCompletedEvent;
 import com.bugzero.rarego.shared.payment.event.SettlementFinishedEvent;
 
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,7 @@ public class PaymentEventKafkaBridge {
 	private final KafkaTemplate<String, Object> kafkaTemplate;
 
 	private static final String TOPIC_SETTLEMENT_FINISHED = "payment-settlement-finished";
+	private static final String TOPIC_AUCTION_PAYMENT_COMPLETED = "payment-auction-completed";
 
 	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
 	public void send(SettlementFinishedEvent event) {
@@ -26,10 +28,20 @@ public class PaymentEventKafkaBridge {
 			return;
 		}
 
-		log.info("Kafka 발생: 정산 완료 ( 총 {} 건, 금액 {}원)", event.totalCount(), event.totalAmount());
+		log.info("Kafka 발행: 정산 완료 ( 총 {} 건, 금액 {}원)", event.totalCount(), event.totalAmount());
 
 		String key = UUID.randomUUID().toString(); // 묶음 처리 건이므로 ID가 없으므로 UUID 이용
 
 		kafkaTemplate.send(TOPIC_SETTLEMENT_FINISHED, key, event);
+	}
+
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void send(AuctionPaymentCompletedEvent event) {
+		log.info("Kafka 발행: 낙찰 결제 완료 (orderId={}, auctionId={})", event.orderId(), event.auctionId());
+
+		// Key를 auctionId로 설정하여, 동일 경매 건에 대한 메시지 순서를 보장
+		String key = String.valueOf(event.auctionId());
+
+		kafkaTemplate.send(TOPIC_AUCTION_PAYMENT_COMPLETED, key, event);
 	}
 }
