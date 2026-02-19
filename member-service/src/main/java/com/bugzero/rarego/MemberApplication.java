4882cd5c538306b61f437054c1bdcb2b1ea083eb
gumguard <gawonlee0712@gmail.com>
2026-02-19 17:23:38 +0900

    chore(auth): application 이름 통일

--- member-service/src/main/java/com/bugzero/rarego/MemberApplication.java
+++ member-service/src/main/java/com/bugzero/rarego/MemberApplication.java
@@ -2,8 +2,10 @@ package com.bugzero.rarego;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MemberServiceApplication {
 
 	public static void main(String[] args) {
