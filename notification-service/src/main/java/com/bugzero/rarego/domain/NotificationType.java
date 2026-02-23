1b792047f7d18d3396cae8caea9f30e039604a70
ward <rlawlsaud2149@gmail.com>
2026-02-23 09:43:04 +0900

    fix(notification): 경매 관련 알림 링크 수정

--- notification-service/src/main/java/com/bugzero/rarego/domain/NotificationType.java
+++ notification-service/src/main/java/com/bugzero/rarego/domain/NotificationType.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum NotificationType {
-	AUCTION_OUTBID("입찰가 추월", "/auction/%d"),
-	BOOKMARK_AUCTION_STARTED("관심 경매 시작", "/auction/%d"),
+	AUCTION_OUTBID("입찰가 추월", "/auctions/%d"),
+	BOOKMARK_AUCTION_STARTED("관심 경매 시작", "/auctions/%d"),
 	AUCTION_WON("경매 낙찰", "/mypage/orders"),
 	AUCTION_PAYMENT_COMPLETED("경매 결제 완료", "/mypage/sales"),
 	AUCTION_PAYMENT_EXPIRING_SOON("낙찰 결제 임박", "/mypage/orders"),
